6ac72441bcf8ad8ed7c0b028372caea6d011f26c
Sachin Patney <sachin.patney@gmail.com>
2019-02-08 13:27:32 -0800

    remove extra semicolen

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,6 @@
 #include "lib/types.h"
 #include "lib/stdlib.h"
-#include "lib/gdt.h";
+#include "lib/gdt.h"
 
 extern "C" void boot(void * multiboot, uint32_t magic) {
     printf("Hello, I am your Operating System. My name is PixelMite.");
