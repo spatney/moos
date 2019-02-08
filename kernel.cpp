dc2da8b174f34c0324c551b00ccd41fb31569a07
Sachin Patney <sachin.patney@gmail.com>
2019-02-07 20:30:14 -0800

    adding GDT

--- kernel.cpp
+++ kernel.cpp
@@ -1,8 +1,11 @@
 #include "lib/types.h"
 #include "lib/stdlib.h"
+#include "lib/gdt.h";
 
 extern "C" void boot(void * multiboot, uint32_t magic) {
     printf("Hello, I am your Operating System");
+
+    GlobalDescriptorTable gdt;
 
     while(1);
 }
