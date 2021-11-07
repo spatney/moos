86dd0a8577bfeae82f6bacf26d79f3c28257ae1d
Sachin Patney <sachin.patney@gmail.com>
2021-11-06 23:29:56 -0700

    calling global constructors

--- loader.s
+++ loader.s
@@ -9,10 +9,12 @@
 
 .section .text
 .extern kernel_main
+.extern callConstructors
 .global loader
 
 loader:
     mov $kernel_stack, %esp
+    call callConstructors
     push %eax
     push %ebx
     call kernel_main
