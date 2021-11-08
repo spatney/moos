73ca26ca1b558dd6519fe14a2d7fdc60bba1aea1
Sachin Patney <sachin.patney@gmail.com>
2021-11-08 14:00:10 -0800

    removing warnings

--- barebones/kernel.cpp
+++ barebones/kernel.cpp
@@ -1,11 +1,10 @@
 extern "C" void kernel_main()
 {
     const char *message = "Hello! this is the Barebones OS. Bye!";
-    short *VideoMemory = (short *)0xb8000;
 
     for (int i = 0; message[i] != '\0'; ++i)
     {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | message[i];
+        ((short *)0xb8000)[i] = (((short *)0xb8000)[i] & 0xFF00) | message[i];
     }
     while (1)
         ;
