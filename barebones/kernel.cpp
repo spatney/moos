6d0b11a698a5fe7b25395b8384ea224c962bfe25
Sachin Patney <sachin.patney@gmail.com>
2021-11-11 11:16:12 -0800

    adding some color to barebones

--- barebones/kernel.cpp
+++ barebones/kernel.cpp
@@ -4,7 +4,10 @@ extern "C" void kernel_main()
 
     for (int i = 0; message[i] != '\0'; ++i)
     {
-        ((short *)0xb8000)[i] = (((short *)0xb8000)[i] & 0xFF00) | message[i];
+        // 2bytes per char, one for color, one for text
+        short color = 0xF300;
+        short colorAlt = 0xFF00;
+        ((short *)0xb8000)[i] = (((short *)0xb8000)[i] & (i % 2 ? color : colorAlt)) | message[i];
     }
     while (1)
         ;
