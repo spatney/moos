087411ab696e280466bd9ab67b97042b717bd15c
Sachin Patney <sachin.patney@gmail.com>
2021-11-10 16:36:16 -0800

    adding graphics mode pre processor directive

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,3 +1,5 @@
+#define GRAPHICS_MODE = 1;
+
 #include <common/types.h>
 #include <common/console.h>
 
@@ -9,9 +11,11 @@
 #include <drivers/keyboard.h>
 #include <drivers/mouse.h>
 
+#ifdef GRAPHICS_MODE
 #include <gui/graphics.h>
 #include <gui/desktop.h>
 #include <gui/window.h>
+#endif
 
 #include <gdt.h>
 
@@ -19,8 +23,12 @@ using namespace moos;
 using namespace moos::hardware;
 using namespace moos::drivers;
 using namespace moos::common;
-using namespace moos::gui;
 
+#ifdef GRAPHICS_MODE
+using namespace moos::gui;
+#endif
+
+#ifndef GRAPHICS_MODE
 class PrintFKeyboardEventHandler : public KeyboardEventHandler
 {
 public:
@@ -40,64 +48,32 @@ public:
 class MouseToCosole : public MouseEventHandler
 {
     int8_t x, y;
-    Desktop *desktop;
-    GraphicsContext *gc;
 
 public:
-    MouseToCosole(Desktop *desktop, GraphicsContext *gc)
+    MouseToCosole()
     {
-        /*x = 40;
+        x = 40;
         y = 12;
-        invertVideoMemoryAt(x, y);*/
-
-        this->desktop = desktop;
-        this->gc = gc;
+        invertVideoMemoryAt(x, y);
     }
     void OnMouseDown(uint8_t button)
     {
-        //invertVideoMemoryAt(x, y);
-
-        desktop->OnMouseDown(button);
+        invertVideoMemoryAt(x, y);
     }
     void OnMouseUp(uint8_t button)
     {
-        //invertVideoMemoryAt(x, y);
-        desktop->OnMouseUp(button);
+        invertVideoMemoryAt(x, y);
     }
     void OnMouseMove(int32_t xOffset, int32_t yOffSet)
     {
-        /*invertVideoMemoryAt(x, y);
-
-        x += xOffset;
-        if (x >= 80)
-        {
-            x = 79;
-        }
-        if (x < 0)
-        {
-            x = 0;
-        }
-        y += yOffSet;
-        if (y >= 25)
-        {
-            y = 24;
-        }
-        if (y < 0)
-        {
-            y = 0;
-        }
-
-        invertVideoMemoryAt(x, y);*/
-
-        desktop->OnMouseMove(xOffset, yOffSet);
-        //desktop->Draw(gc);
+        invertVideoMemoryAt(x, y);
     }
     void invertVideoMemoryAt(int8_t x, int8_t y)
     {
         VideoMemory[80 * y + x] = (VideoMemory[80 * y + x] & 0x0F00) << 4 | (VideoMemory[80 * y + x] & 0xF000) >> 4 | (VideoMemory[80 * y + x] & 0x00FF);
     }
 };
-
+#endif
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
@@ -120,16 +96,24 @@ extern "C" void kernel_main(void *multiboot, uint32_t magic)
     PeripheralComponentInterconnectController pciController;
     pciController.SelectDrivers(&driverManager, &interruptManager);
 
+    MouseEventHandler *handler;
+    
+#ifndef GRAPHICS_MODE
     PrintFKeyboardEventHandler keyboardEventHandler;
     KeyboardDriver keyboard(&interruptManager, &keyboardEventHandler);
     driverManager.AddDriver(&keyboard);
 
+    MouseToCosole mouseToConsole;
+    handler = &mouseToConsole;
+#endif
+
+#ifdef GRAPHICS_MODE
     GraphicsContext gc;
     Desktop desktop(320, 200, 0xFF, 0xFF, 0xFF);
+    handler = &desktop;
+#endif
 
-    MouseToCosole mouseToConsole(&desktop, &gc);
-
-    MouseDriver mouse(&interruptManager, &mouseToConsole);
+    MouseDriver mouse(&interruptManager, handler);
     driverManager.AddDriver(&mouse);
 
     Console::Write("Activating driver manager ...\n");
@@ -140,18 +124,22 @@ extern "C" void kernel_main(void *multiboot, uint32_t magic)
 
     Console::Write("\n\nMoOS\a> ");
 
+#ifdef GRAPHICS_MODE
     Window win1(&desktop, 10, 10, 20, 20, 0xA8, 0, 0);
     Window win2(&desktop, 40, 40, 30, 30, 0, 0xA8, 0);
-    Window win3(&desktop, 100, 150, 20, 20, 0,0, 0xA8);
+    Window win3(&desktop, 100, 150, 20, 20, 0, 0, 0xA8);
     Window win4(&desktop, 200, 150, 30, 30, 0xA8, 0xA8, 0);
 
     desktop.AddChildWidget(&win1);
     desktop.AddChildWidget(&win2);
     desktop.AddChildWidget(&win3);
     desktop.AddChildWidget(&win4);
+#endif
 
     while (1)
     {
+#ifdef GRAPHICS_MODE
         desktop.Draw(&gc);
+#endif
     }
 }
